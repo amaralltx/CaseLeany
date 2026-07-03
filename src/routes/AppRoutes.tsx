15dd5fc3429ca4efb31af39f3ce213842c8d1c8a
Lucas Amaral <91980141+amaralltx@users.noreply.github.com>
2026-07-03 03:22:01 -0300

    hotfix(routes): add pokedex as first page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,8 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { PokedexPage } from '../pages/PokedexPage/PokedexPage';
 import { RegionsPage } from '../pages/RegionsPage/RegionsPage';
 import { RegionDetailPage } from '../pages/RegionDetailPage/RegionDetailPage';
-import { FavoritesPage } from '../pages/FavoritesPage/FavoritesPage';
 import { PokemonDetailPage } from '../pages/PokemonDetailPage/PokemonDetailPage';
+import { FavoritesPage } from '../pages/FavoritesPage/FavoritesPage';
 
 export function AppRoutes() {
   return (
@@ -14,6 +14,7 @@ export function AppRoutes() {
       <Route path="/regions/:regionId" element={<RegionDetailPage />} />
       <Route path="/pokemon/:pokemonId" element={<PokemonDetailPage />} />
       <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="*" element={<Navigate to="/pokedex" replace />} />
     </Routes>
   );
 }
